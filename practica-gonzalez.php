2840336c6bb2a267761e9c3f952127a5aa11878b
Jose Antonio Gonzalez Castilla <joseantgc81@gmail.com>
2025-04-20 16:29:00 +0200

    Primer commit del proyecto documentacion_php

---
+++ practica-gonzalez.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Este script contiene funciones de ejemplo con documentación en formato DocBlock.
+ *
+ * @author Jose Antonio Gonzalez
+ * @version 1.0
+ * @internal Este script es solo para fines educativos.
+ */
+
+/**
+ * Calcula la suma de dos números.
+ *
+ * @param float $a El primer número.
+ * @param float $b El segundo número.
+ * @return float La suma de los dos números.
+ */
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+/**
+ * Convierte una cadena a mayúsculas.
+ *
+ * @param string $texto La cadena de texto a convertir.
+ * @return string La cadena en mayúsculas.
+ */
+function convertirAMayusculas($texto) {
+    return strtoupper($texto);
+}
+?>
+
+
